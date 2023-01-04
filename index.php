0c451dbbb3bd92e4cb412dae5497c39edab6cf90
Hiramon Nahar Eva <hiramonnahareva@gmail.com>
2023-01-04 15:43:08 +0600

    add opject

--- index.php
+++ index.php
@@ -9,13 +9,74 @@
 <body>
     <?php echo 'hello php'; 
 
-    // add veriable
+    // how to write veriable in php
 
     $food = 'pizza';
     echo 'I love to eat ' . $food . '<br>'; 
-    $age = 10;
-    // $_age = 19;
-    echo 'My age is' . $age;
+    
+    // How to write var_dump function 
+    
+
+    // Note: var_dump() function dumps information holds type and value of the variable(s).
+
+    // differents between echo & print 
+    /* 
+    1. echo is marginally faster than print
+    2. echo has no return value while print has a return value of 1
+     */
+
+     $river = 'padma';
+     $rivers = print $river;
+     echo($rivers);
+     $name = 'elma';
+     $name1 = "it's me";
+     var_dump ($name);
+    //  
+     $age = 20;
+     $age = 18.2;
+
+    // sum
+
+    $a = 2;
+    $b = 4;
+    echo $a + $b . '<br>';
+
+    // or
+
+    $sum = ($a + $b) . '<br>';
+
+    echo $sum;
+
+     // multiple
+
+     $a = 2;
+     $b = 4;
+     echo $a - $b . '<br>';
+ 
+     // or
+ 
+     $sum = ($a + $b) . '<br>';
+ 
+     echo $sum;
+
+    //   How to wirte object 
+    
+    
+     echo(min(0, 15, 30, 50, 50, 85, 85, 17));
+
+     class phone {
+        var $model;
+        function phoneModel ($number){
+            global $model;
+            $model = $number;
+            echo "this is $model <br>";
+        }
+    }
+    $apple = new phone;
+    $apple-> phoneModel('iphone 13');
+
+    
+
     ?>
 
 </body>
