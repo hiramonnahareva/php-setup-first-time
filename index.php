15dfe04298db5a02b3323620117b0c3229bfcffa
Hiramon Nahar Eva <hiramonnahareva@gmail.com>
2023-01-08 19:43:34 +0600

    add number concept

--- index.php
+++ index.php
@@ -73,6 +73,8 @@
     $apple = new phone;
     $apple-> phoneModel('iphone 13');
 
+
+
     //1. To Return the length of the string 
 
     echo strlen('I love to eat pizza') . '<br>';
@@ -89,18 +91,33 @@
     characters with some other characters in a string.  */
 
     echo str_replace("world", "Dolly", "Hello world!");
-
-
     
     // echo str_shuffle('I love to eat pizza') . '<br>';
+ 
 
 
     // The pi() function returns the value of PI:
 
-    echo pi() . '<br>';
+    echo pi() . '<br>'; // returns 3.1415926535898
 
     define('a', 19);
     echo(a);
+    echo(min(0, 150, 30, 20, -8, -200));  // returns -200
+    echo(max(0, 150, 30, 20, -8, -200));  // returns 150
+
+    // The abs() function returns the absolute (positive) value of a number 
+
+    echo(abs(-5));  // returns 5
+
+
+
+
+
+
+
+
+
+    
 
     // How to define a constant on PHP
 
@@ -113,7 +130,70 @@
         'Asus',
         'Apple',
     ]);
-    var_dump(Laptop)
+
+    echo(Laptop[0]);
+
+    // The rand() function generates a random number
+
+
+    // var_dump(Laptop)
+
+
+
+
+
+
+    # Number
+
+    // 1. Check if the type of a variable is integer 
+
+        $x = 5985;
+        var_dump(is_int($x)); // returns boolean true
+
+    // 2. Check if the type of a variable is float 
+
+        $x = 10.365;
+        var_dump(is_float($x)); // returns boolean true
+
+
+
+
+
+
+
+
+
+
+
+
+    // The pi() function returns the value of PI 
+
+        echo(pi()) .'<br>'; // returns 3.1415926535898
+
+    // The min() and max() functions can be used to find the lowest or highest value in a list of arguments 
+
+    echo(min(0, 1, 20, -8, -200));  // returns -200
+    echo(max(0, 150, 30, 20, -8, -200)) . '<br>';  // returns 150
+
+    // The absolute (positive) value of a number 
+
+    echo(abs(-6));  // returns 6
+
+    // The sqrt() function returns the square root of a number
+
+    echo(sqrt(64));  // returns 8
+
+    // he round() function rounds a floating-point number to its nearest integer 
+
+    echo(round(5.90)); // returns 6
+    // or
+    echo(round(5.20)) ; // returns 5
+
+   // The rand() function generates a random number 
+   echo(rand(10, 100));
+
+
+    
     
 
     ?>
