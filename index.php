d01385a54a53b6547071cbcf0e7c2e0f917bfd15
Hiramon Nahar Eva <hiramonnahareva@gmail.com>
2023-01-16 11:52:47 +0600

    add switch

--- index.php
+++ index.php
@@ -202,6 +202,22 @@ if ($t < "20") {
 } else {
   echo "Have a good night!";
 }
+
+$favcolor = "red";
+
+switch ($favcolor) {
+  case "red":
+    echo "Your favorite color is red!";
+    break;
+  case "blue":
+    echo "Your favorite color is blue!";
+    break;
+  case "green":
+    echo "Your favorite color is green!";
+    break;
+  default:
+    echo "Your favorite color is neither red, blue, nor green!";
+}
     
     
 
