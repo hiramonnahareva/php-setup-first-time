b38810ba27227ff831ac23bcb49b4eded3b4c29f
Hiramon Nahar Eva <hiramonnahareva@gmail.com>
2023-01-21 00:31:17 +0600

    add Operators

--- index.php
+++ index.php
@@ -232,6 +232,13 @@ if ($t < "20") {
 $cars = array("Volvo", "BMW", "Toyota");
 echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
 
+/* --------------- Operators ---------------- */
+echo 10 + 5 .'<br>';
+echo 10 - 5 .'<br>';
+echo 10 * 5 .'<br>';
+echo 10 / 5 .'<br>';
+echo 10 % 5 .'<br>';
+
     ?>
 
 </body>
