b0fea6ee9f6d7e4f267f8700de74aaf6496fc6b5
Hiramon Nahar Eva <hiramonnahareva@gmail.com>
2023-02-03 23:23:26 +0600

    echo statement

--- index.php
+++ index.php
@@ -319,7 +319,12 @@ var_dump($x <= $y); //return  false
 $date=date_create("2013-03-15");
 date_add($date,date_interval_create_from_date_string("40 days"));
 echo date_format($date,"Y-m-d");
-?>
+
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+
 
 
 
