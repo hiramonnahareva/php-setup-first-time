756a0126b2378195f99f4bf72639de5c6ee1bafb
Hiramon Nahar Eva <hiramonnahareva@gmail.com>
2023-02-05 01:15:54 +0600

    PHP form

--- index.php
+++ index.php
@@ -340,5 +340,11 @@ interface InterfaceName {
 
     ?>
 
+    <form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
 </body>
 </html>
