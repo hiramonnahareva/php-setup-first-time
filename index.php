ab9ea2ee34cae30cce63b7f04e30750e2ec02d9f
Hiramon Nahar Eva <96996000+hiramonnahareva@users.noreply.github.com>
2023-03-11 22:24:48 +0600

    Update index.php

--- index.php
+++ index.php
@@ -381,12 +381,6 @@ check
 
     ?>
 
-    <form action="welcome.php" method="post">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
-<input type="submit">
-</form>
-
 
 </body>
 </html>
