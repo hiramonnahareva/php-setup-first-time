3c3b306c40df221473eca62341ba629120f6834c
Hiramon Nahar Eva <96996000+hiramonnahareva@users.noreply.github.com>
2023-03-30 23:56:29 +0600

    Update index.php

--- index.php
+++ index.php
@@ -372,7 +372,17 @@ if(!isset($_COOKIE[$cookie_name])) {
 $_SESSION["favcolor"] = "green";
 $_SESSION["favanimal"] = "cat";
 echo "Session variables are set.";
+// PHP Filters Advanced
+	
+	$int = 122;
+$min = 1;
+$max = 200;
 
+if (filter_var($int, FILTER_VALIDATE_INT, array("options" => array("min_range"=>$min, "max_range"=>$max))) === false) {
+  echo("Variable value is not within the legal range");
+} else {
+  echo("Variable value is within the legal range");
+}
 ?>
 
 
