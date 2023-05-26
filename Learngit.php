94a505c0166df6c302cb3d7ff8b381d1ce22e9ac
Hiramon Nahar Eva <96996000+hiramonnahareva@users.noreply.github.com>
2023-05-26 17:10:11 +0600

    Update Learngit.php

--- Learngit.php
+++ Learngit.php
@@ -8,9 +8,4 @@ if (filter_var($int, FILTER_VALIDATE_INT, array("options" => array("min_range"=>
 } else {
   echo("Variable value is within the legal range");
 }
-if (filter_var($int, FILTER_VALIDATE_INT, array("options" => array("min_range"=>$min, "max_range"=>$max))) === false) {
-  echo("Variable value is not within the legal range");
-} else {
-  echo("Variable value is within the legal range");
-}
 ?>
